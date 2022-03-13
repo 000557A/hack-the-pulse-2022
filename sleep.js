f579c3b1ff7bba15aa3679ff83a7aa528f0e4fe7
Brandon <98434819+Brandon2023@users.noreply.github.com>
2022-03-12 22:59:41 -0800

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,4 +1,3 @@
-
 window.onload = () => {
     let button = document.querySelector("#btn");
 
@@ -17,22 +16,20 @@ function calculateSleep() {
     if (age >= 1 || age <= 2) 
         result.innerHTML = "You need 12-16 hours of sleep every day.";
   
-    else if (age >= 3 || age <= 5) 
+        else if (age >= 3 || age <= 5) 
         result.innerHTML = "You need 11-14 hours of sleep every day.";
-  
-    else if (age >= 6 || age <= 12) 
+    
+        else if (age >= 6 || age <= 12) 
         result.innerHTML = "You need 9-12 hours of sleep every day.";
-    else if (age >= 13 || age <= 18) 
+        else if (age >= 13 || age <= 18) 
         result.innerHTML = "You need 8-10 hours of sleep every day.";
-    else if (age >= 19 || age <= 60) 
+        else if (age >= 19 || age <= 60) 
         result.innerHTML = "You need 7 or more hours of sleep every day.";
-    else if (age >= 61 || age <= 64) 
+        else if (age >= 61 || age <= 64) 
         result.innerHTML = "You need 7-9 hours of sleep every day.";
-    else if (age >= 65) 
+        else if (age >= 65) 
         result.innerHTML = "You need 7-9 hours of sleep every day.";
-    else if (age >= 100)
-        result.innerHTML = "How are you not dead? Ayy You probably don't even need sleep anymore at your age";
-    else {
+        else {
   
        result.innerHTML = "You need to input a proper value for age."
     }
