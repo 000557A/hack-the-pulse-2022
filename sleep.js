393a7033f8bdf695df02757ca7992e20b9d78943
Santo Neyyan <santoneyyan3@gmail.com>
2022-03-12 23:02:45 -0800

    f

--- sleep.js
+++ sleep.js
@@ -22,16 +22,22 @@ function calculateSleep() {
   
     else if (age >= 6 || age <= 12) 
         result.innerHTML = "You need 9-12 hours of sleep every day.";
+
     else if (age >= 13 || age <= 18) 
         result.innerHTML = "You need 8-10 hours of sleep every day.";
+
     else if (age >= 19 || age <= 60) 
         result.innerHTML = "You need 7 or more hours of sleep every day.";
+
     else if (age >= 61 || age <= 64) 
         result.innerHTML = "You need 7-9 hours of sleep every day.";
+
     else if (age >= 65) 
         result.innerHTML = "You need 7-9 hours of sleep every day.";
+
     else if (age >= 100)
         result.innerHTML = "How are you not dead? Ayy You probably don't even need sleep anymore at your age";
+        
     else {
   
        result.innerHTML = "You need to input a proper value for age."
