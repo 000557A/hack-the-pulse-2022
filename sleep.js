6ee5b4d6d25f55e8ff654d81364f05acbfca1b2c
000557A <101441923+000557A@users.noreply.github.com>
2022-03-12 23:27:04 -0800

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -16,25 +16,25 @@ function calculateSleep() {
     if (age >= 1, age <= 2) 
         result.innerHTML = "You need 12-16 hours of sleep every day.";
   
-    if (age >= 3, age <= 5) 
+    else if (age >= 3, age <= 5) 
         result.innerHTML = "You need 11-14 hours of sleep every day.";
     
-    if (age >= 6, age <= 12) 
+    else if (age >= 6, age <= 12) 
         result.innerHTML = "You need 9-12 hours of sleep every day.";
 
-    if (age >= 13, age <= 18) 
+    else if (age >= 13, age <= 18) 
         result.innerHTML = "You need 8-10 hours of sleep every day.";
 
-    if (age >= 19, age <= 60) 
+    else if (age >= 19, age <= 60) 
         result.innerHTML = "You need 7 or more hours of sleep every day.";
 
-    if (age >= 61, age <= 64) 
+    else if (age >= 61, age <= 64) 
         result.innerHTML = "You need 7-9 hours of sleep every day.";
 
-    if (age = 65) 
+    else if (age = 65) 
         result.innerHTML = "You need 7-9 hours of sleep every day.";
 
-        else {
+    else {
   
        result.innerHTML = "You need to input a proper value for age."
     }
