88c86d85fad9d527b16bfaf752b862a6ec5abd5e
Santo Neyyan <santoneyyan3@gmail.com>
2022-03-12 22:43:33 -0800

    fda

--- sleep.js
+++ sleep.js
@@ -28,19 +28,6 @@ function calculateSleep() {
     result.innerHTML = "You need 11-14 hours of sleep every day.";
     else {
   
-        
-        let bmi = (weight / ((height * height) 
-                            / 10000)).toFixed(2);
-  
-        
-        if (bmi < 18.6) result.innerHTML =
-            `Under Weight : <span>${bmi}</span>`;
-  
-        else if (bmi >= 18.6 && bmi < 24.9) 
-            result.innerHTML = 
-                `Normal : <span>${bmi}</span>`;
-  
-        else result.innerHTML =
-            `Over Weight : <span>${bmi}</span>`;
+       result.innerHTML = "no worko"
     }
 }
